c333f3e76cd4a7c2678b12c8ee21e14de3e03006
Sophia Kim <mail@fifikim.com>
2022-04-12 14:54:08 -0400

    add users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_12_074256) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_12_174839) do
   create_table "uploads", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_074256) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.integer "views", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
