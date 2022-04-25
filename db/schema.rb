8d7f64616734c6dd3505afea925356a87d0b6411
Sophia Kim <mail@fifikim.com>
2022-04-24 21:44:18 -0400

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_15_154839) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -56,7 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_154839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "views", default: 0
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "file"
     t.index ["user_id"], name: "index_uploads_on_user_id"
   end
