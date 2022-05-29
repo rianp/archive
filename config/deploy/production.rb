38d0f80992c009e68113c5cfd21b10053798d5d9
Ryan Pickell <rianchristopher@gmail.com>
2022-05-29 14:48:57 -0700

    deploy edits

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '143.198.229.107', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
